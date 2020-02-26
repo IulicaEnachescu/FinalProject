96b0fd158547d02d4a1891a135b8ad6d2db8a989
IulicaEnachescu <iulica.enachescu@gmail.com>
2020-02-26 21:22:48 +0200

    m

---
+++ SchoolWebApp/SchoolWebApp.Services/IServices/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolWebApp.Services.IServices
+{
+    public interface IUserService
+    {
+        bool UserNameNotFound(string name);
+    }
+}
---
+++ SchoolWebApp/SchoolWebApp.Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using SchoolWebApp.Services.Interfaces;
+using SchoolWebApp.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolWebApp.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+        public bool UserNameNotFound(string name)
+        {
+            var totUsers = this._userRepository.GetAll();
+            var el=totUsers.FirstOrDefault(x =>(x.UserName == name));
+            return (Object.Equals(el, null));
+
+        }
+    }
+}
